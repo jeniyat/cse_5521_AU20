56b92bdab912a857c7b85262da4d2934f3e78c0d
Jeniya Tabassum <jeniya.tabassum@gmail.com>
2020-09-04 10:42:09 -0400

    updated util.py

--- programming_homeworks/HW_1_programming/py/util.py
+++ programming_homeworks/HW_1_programming/py/util.py
@@ -54,28 +54,41 @@ class Queue:
 
 class PriorityQueue:
     """
-    Implements a priority queue data structure. Each inserted item
-    has a priority associated with it and the client is usually interested
-    in quick retrieval of the lowest-priority item in the queue. This
-    data structure allows O(1) access to the lowest-priority item.
-
-    Note that this PriorityQueue does not allow you to change the priority
-    of an item.  However, you may insert the same item multiple times with
-    different priorities.
+      Implements a priority queue data structure. Each inserted item
+      has a priority associated with it and the client is usually interested
+      in quick retrieval of the lowest-priority item in the queue. This
+      data structure allows O(1) access to the lowest-priority item.
     """
     def  __init__(self):
         self.heap = []
+        self.count = 0
 
     def push(self, item, priority):
-        pair = (priority,item)
-        heapq.heappush(self.heap,pair)
+        entry = (priority, self.count, item)
+        heapq.heappush(self.heap, entry)
+        self.count += 1
 
     def pop(self):
-        (priority,item) = heapq.heappop(self.heap)
+        (_, _, item) = heapq.heappop(self.heap)
         return item
 
     def isEmpty(self):
         return len(self.heap) == 0
+
+    def update(self, item, priority):
+        # If item already in priority queue with higher priority, update its priority and rebuild the heap.
+        # If item already in priority queue with equal or lower priority, do nothing.
+        # If item not in priority queue, do the same thing as self.push.
+        for index, (p, c, i) in enumerate(self.heap):
+            if i == item:
+                if p <= priority:
+                    break
+                del self.heap[index]
+                self.heap.append((priority, c, item))
+                heapq.heapify(self.heap)
+                break
+        else:
+            self.push(item, priority)
 
 class PriorityQueueWithFunction(PriorityQueue):
     """
