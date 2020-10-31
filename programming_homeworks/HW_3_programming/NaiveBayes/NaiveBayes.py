2f931de1801630db69a843105b9ff4193396d01d
Jeniya Tabassum <jeniya.tabassum@gmail.com>
2020-10-31 16:35:08 -0600

    updated readme

--- programming_homeworks/HW_3_programming/NaiveBayes/NaiveBayes.py
+++ programming_homeworks/HW_3_programming/NaiveBayes/NaiveBayes.py
@@ -1,10 +1,6 @@
 from collections import defaultdict
 import numpy as np
 
-
-
-
-
 def file_reader(file_path, label):
     list_of_lines = []
     list_of_labels = []
@@ -39,78 +35,73 @@ def data_reader(source_directory):
     return list_of_all_lines, list_of_all_labels
 
 
-
-    
-    
-
 def evaluate_predictions(test_set,test_labels,trained_classifier):
-  correct_predictions = 0
-  predictions_list = []
-  prediction = -1
-
-  for dataset,label in zip(test_set, test_labels):
-    probabilities = trained_classifier.predict(dataset)
-    if probabilities[0] >= probabilities[1]:
-      prediction = 0
-    elif  probabilities[0] < probabilities[1]:
-      prediction = 1
+    correct_predictions = 0
+    predictions_list = []
+    prediction = -1
+    for dataset,label in zip(test_set, test_labels):
+        probabilities = trained_classifier.predict(dataset)
+        if probabilities[0] >= probabilities[1] and probabilities[0] >= probabilities[-1]:
+            prediction = 0
+        elif  probabilities[1] >= probabilities[0] and probabilities[1] >= probabilities[-1]:
+            prediction = 1
+        else:
+            prediction=-1
+        if prediction == label:
+            correct_predictions += 1
+            predictions_list.append("+")
+        else:
+            predictions_list.append("-")
     
-    if prediction == label:
-      correct_predictions += 1
-      predictions_list.append("+")
-    else:
-      predictions_list.append("-")
+    print("Total Sentences correctly: ", len(test_labels))
+    print("Predicted correctly: ", correct_predictions)
+    print("Accuracy: {}%".format(round(correct_predictions/len(test_labels)*100,5)))
 
-  
-  print("Total Sentences correctly: ", len(test_labels))
-  print("Predicted correctly: ", correct_predictions)
-  print("Accuracy: {}%".format(round(correct_predictions/len(test_labels)*100,5)))
-  return predictions_list, round(correct_predictions/len(test_labels)*100)
+    return predictions_list, round(correct_predictions/len(test_labels)*100)
 
 
-
-# noinspection SpellCheckingInspection
 class NaiveBayesClassifier(object):
     def __init__(self, n_gram=1, printing=False):
         self.prior = defaultdict(int)
         self.logprior = {}
-        self.training_documents = defaultdict(list)
+        self.train_data = defaultdict(list)
         self.loglikelihoods = defaultdict(defaultdict)
         self.V = []
         self.n = n_gram
 
     
-    def compute_vocabulary(self, documents):
+
+    def compute_vocabulary(self, training_sentences):
         vocabulary = set()
 
-        for doc in documents:
-            for word in doc.split(" "):
+        for sentence in training_sentences:
+            for word in sentence.split(" "):
                 vocabulary.add(word.lower())
 
         return vocabulary
 
     def count_word_in_classes(self):
         counts = {}
-        for c in list(self.training_documents.keys()):
-            docs = self.training_documents[c]
+        for c in list(self.train_data.keys()):
+            sentences = self.train_data[c]
             counts[c] = defaultdict(int)
-            for doc in docs:
-                words = doc.split(" ")
+            for sent in sentences:
+                words = sent.split(" ")
                 for word in words:
                     counts[c][word] += 1
 
         return counts
 
-    def train(self, training_set, training_labels, alpha=1):
-        # Get number of documents
-        N_doc = len(training_set)
+    def train(self, training_sentences, training_labels, alpha=1):
+        # Get number of sentences in the training set
+        N_sentences = len(training_sentences)
 
         # Get vocabulary used in training set
-        self.V = self.compute_vocabulary(training_set)
+        self.V = self.compute_vocabulary(training_sentences)
 
-        # Create training_documents
-        for x, y in zip(training_set, training_labels):
-            self.training_documents[y].append(x)
+        # Create thre complete training data by combining the training labels with the training sentences
+        for x, y in zip(training_sentences, training_labels):
+            self.train_data[y].append(x)
 
         # Get set of all classes
         all_classes = set(training_labels)
@@ -118,51 +109,45 @@ class NaiveBayesClassifier(object):
         # Compute a dictionary with all word counts for each class
         self.word_count = self.count_word_in_classes()
 
-        
+
         #-----------------------#
         #-------- TO DO --------#
         #-------- TO DO --------#
 
         #compute the log likelihood and priors from traing data
 
-    def predict(self, test_doc):
+        
+
+    def predict(self, test_sentence):
         sums = {
             0: 0,
             1: 0,
-            -1:0,
         }
+        words = test_sentence.split(" ")
+
         #-----------------------#
         #-------- TO DO --------#
         #-------- TO DO --------#
           
 
-        # return a dictionary of log probablity for each class for a given test document: 
+        # return a dictionary of log probablity for each class for a given test sentence: 
         # i,e, {0: -39.39854137691295, 1: -41.07638511893377, -1: -42.93948478571315}
+
         return sums
 
 
 if __name__ == '__main__':
-    
     train_folder = "data-sentiment/train/"
     test_folder = "data-sentiment/test/"
 
-
-    training_set, training_labels = data_reader(train_folder)
-    test_set, test_labels = data_reader(test_folder)
-
-
+    training_sentences, training_labels = data_reader(train_folder)
+    test_sentences, test_labels = data_reader(test_folder)
 
     NBclassifier = NaiveBayesClassifier(n_gram=1)
-    NBclassifier.train(training_set,training_labels)
+    NBclassifier.train(training_sentences,training_labels)
 
 
-
-
-
-
-
-
-    results, acc = evaluate_predictions(test_set, test_labels, NBclassifier)
+    results, acc = evaluate_predictions(test_sentences, test_labels, NBclassifier)
 
 
 
--- programming_homeworks/HW_3_programming/Readme.md
+++ programming_homeworks/HW_3_programming/Readme.md
@@ -2,12 +2,11 @@
 
 ## Submission instructions
 
-* Due date and time: November 16 (Monday), 11:59 pm ET
+* Due date and time: November 20 (Monday), 11:59 pm ET
 
 * Carmen submission: 
 Submit a .zip file named `name.number.zip` (e.g., `chao.209.zip`), which contains the following files
-  - your completed python script `NaiveBayes.py` (for NaiveBayes - Question 1) [Bounus: Your report on impact of alpha with optimal alpha as `report.txt`]
-  - your completed python script `hmm.py` (for Hidden Markov Model - Question 2)
+  - your completed python script `NaiveBayes.py` 
  
 * Collaboration: You may discuss the homework with your classmates. However, you need to write your own solutions and submit them separately. In your submission, you need to list with whom you have discussed the homework. Please list each classmate's name and name.number (e.g., Wei-Lun Chao, chao.209) as a row at the end of `NaiveBayes.py` and `hmm.py`. Please consult the syllabus for what is and is not acceptable collaboration.
 
@@ -15,11 +14,10 @@ Submit a .zip file named `name.number.zip` (e.g., `chao.209.zip`), which contain
 
 * Download or clone this repository.
 
-* You will see two directories: `NaiveBayes` , `HMM`
+* You will see the directory named `NaiveBayes` 
 
-* You will see a [`data-sentiment`](`NaiveBayes/data-sentiment/`) folder inside the  [`NaiveBayes/`](./NaiveBayes/) directory, which contains train data (`train/Positive.txt`, `train/Neutral.txt`, `train/Negative.txt`) and the test data (`test/Positive.txt`, `test/Neutral.txt`, `test/Negative.txt`) for  Question 1.
+* You will see a [`data-sentiment`](`NaiveBayes/data-sentiment/`) folder inside the  [`NaiveBayes/`](./NaiveBayes/) directory, which contains train data (`train/Positive.txt`, `train/Neutral.txt`, `train/Negative.txt`) and the test data (`test/Positive.txt`, `test/Neutral.txt`, `test/Negative.txt`).
 
-* You will see a [`data-pos`](`./HMM/data-pos/`) folder inside the [`HMM`](./HMM/) directory, which contains train data (`train.txt`,  `train_words.txt`) and the test data (`test.txt`,  `test_words.txt`) for  Question 2.
 
 * Please use python3 and write your own solutions from scratch. 
 
@@ -33,14 +31,12 @@ Submit a .zip file named `name.number.zip` (e.g., `chao.209.zip`), which contain
 
 # Introduction
 
-In this homework, you are to implement NaiveBayes and Hidden Markov Model (HMM) algorithm for tweet classifications.
-
-* In Question 1, you will play with the Twitter Senitment Analysis data, where each tweet is tagged as with either Positive, Negative, or Neutral sentiment.
-
-* In Question 2, you will play with Twitter pos data data, where each word in the tweet is tagged corresponding part-of-speech tag.
+In this homework, you are to implement NaiveBayes algorithm for tweet classifications. You will play with the Twitter Senitment Analysis data, where each tweet is tagged as with either Positive, Negative, or Neutral sentiment.
 
 
-# Question 1: NaiveBayes Classification (50 pts)
+# Data Description
+
+# NaiveBayes Classification (50 pts)
 
 * You will implement NaiveBayes in this question. You are to amend your implementation into [`NaiveBayes.py`](./NaiveBayes/NaiveBayes.py).
 
@@ -58,34 +54,11 @@ In this homework, you are to implement NaiveBayes and Hidden Markov Model (HMM) 
 
 * Again, the auto_grader is just to simply check your implementation. It may not be used for your final grading.
 
-## Bonus: (10 pts)
 
-Improve the results on test set with different alpha value in [`def train(self, training_set, training_labels, alpha=1)`](./NaiveBayes/NaiveBayes.py#L104) and report the best alpha at `report.txt`.
-
-
-
-# Question 2: HMM Tagging (50 pts)
-
- You will implement hidden markov model in this question. You are to amend your implementation into `hmm.py`.
-
-* There are many classe and sub-functions in [`hmm.py`](./HMM/hmm.py). You can ignore all of them but [`def get_transition(self, prevNodes, curNodes)`](./HMM/hmm.py#L81). You need to extract the correspondig transition and emmision probablities to complete this implementaion.
-  
-
-## Auto grader:
-
-* You may run the following command to test your implementation<br/>
-`python3 hmm.py`<br/>
-
-* Note that, the auto grader is to check your implementation semantics. If you have syntax errors, you may get python error messages before you can see the auto_graders' results.
-
-* Again, the auto_grader is just to simply check your implementation. It may not be used for your final grading.
-
-* Debugging Tips: print the probability values while updating [`best_prev`](./HMM/hmm.py#L101) variable and check it is assigned with the the highest probability ones.
 
 
 # What to submit:
 
 * Your completed python script `NaiveBayes.py`
-* Your report on impact of alpha with optimal alpha as `report.txt` (Bonus)
-* Your completed python script `hmm.py`
+
 
